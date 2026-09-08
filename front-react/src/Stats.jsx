6833f5c9d634548ace2756175b491d1f86281f4b
leika127 <afrx35@gmail.com>
2026-09-08 12:34:47 +0200

    fix stats.jsx

---
+++ front-react/src/Stats.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+
+import './Stats.css'
+
+function Stats({}) {
+    const [stats, setStats] = useState({})
+
+    //TODO fonction qui récupère les stats depuis l'API et les stocke dans le state
+    
+    const changerStats = async () => {
+        const reponse = await fetch('http://localhost:3000/stats')  
+        const mes_stats = await reponse.json()
+        setStats(mes_stats)
+    }
+
+    chargerStats();
+
+
+    //TODO afficher les stats dans le tableau avec un Map
+
+  return (
+    <>
+      <div className="stats">
+        <h1>Tableau de bord</h1>
+        <h2>Objets par statut</h2>
+        {stats.objets_par_statut .map(o => {
+            return (
+               <div className="carte_stats"> 
+                <p>o.statut</p>
+                <p>o.count</p>
+            </div>
+            )
+
+        })}
+        <div className="carte_stats"> 
+            <p>Poids total reçu</p>
+            <p>{stats.poids_total_recu}</p>
+        </div>
+        
+      </div>
+    </>
+  )
+}
+
+export default Stats
