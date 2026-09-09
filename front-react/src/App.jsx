801a962a654af50c51e48ecf608a820740592959
leika127 <afrx35@gmail.com>
2026-09-09 10:29:31 +0200

    fix app.jsx et stats.jsx

--- front-react/src/App.jsx
+++ front-react/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-
+import Stats from './Stats.jsx'
 import './App.css'
 
 function App() {
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <>
-      <Stats stats={stats} />
+      <Stats/>
 
 
     </>
--- front-react/src/Stats.jsx
+++ front-react/src/Stats.jsx
@@ -1,19 +1,24 @@
-import { useState } from 'react'
-
+import { useState, useEffect } from 'react'
 import './Stats.css'
 
-function Stats({}) {
-    const [stats, setStats] = useState({})
-
+function Stats() {
+    const [stats, setStats] = useState({"objets_par_statut" : []})
     //TODO fonction qui récupère les stats depuis l'API et les stocke dans le state
     
-    const changerStats = async () => {
-        const reponse = await fetch('http://localhost:3000/stats')  
-        const mes_stats = await reponse.json()
-        setStats(mes_stats)
-    }
+    useEffect(() => {
+      const chargerStats = async () => {
+          const reponse = await fetch('http://localhost:3000/api/stats')
+          const mes_stats = await reponse.json()
+          setStats(mes_stats)
+      }
+     const intervalID = setInterval(chargerStats,2000)
+  
+      chargerStats();
+    }, [])
 
-    chargerStats();
+    useEffect(() => {
+      console.log("stats : ", stats)
+    }, [stats])
 
 
     //TODO afficher les stats dans le tableau avec un Map
@@ -23,15 +28,14 @@ function Stats({}) {
       <div className="stats">
         <h1>Tableau de bord</h1>
         <h2>Objets par statut</h2>
-        {stats.objets_par_statut .map(o => {
-            return (
-               <div className="carte_stats"> 
-                <p>o.statut</p>
-                <p>o.count</p>
+        {stats.objets_par_statut.map(o => ( 
+               <div key={o.statut} className="carte_stats"> 
+                <p>{o.statut}</p>
+                <p>{o.count}</p>
             </div>
-            )
+        )
 
-        })}
+        )}
         <div className="carte_stats"> 
             <p>Poids total reçu</p>
             <p>{stats.poids_total_recu}</p>
