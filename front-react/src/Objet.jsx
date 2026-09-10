1196e24fd8901b059b26b4ae2ea309692903a55e
Julien M <julien.morille@hotmail.fr>
2026-09-10 15:10:19 +0200

    filtres catégorie + statut

--- front-react/src/Objet.jsx
+++ front-react/src/Objet.jsx
@@ -1,19 +1,29 @@
 import "./Objet.css";
 
 function Objet({ objets }) {
+  
   return (
     <section className="afficherObjets">
       <ul className="Objet">
         {objets.map((item) => (
           <li key={item.id}>
-            <p> {item.libelle}</p>
-            <p className={`statut-${item.statut}`}> {item.statut} </p>
-            <p> {item.prix} € </p>
+            <details>
+              <summary>
+                <p> {item.libelle}</p>
+                <p className={`statut-${item.statut}`}> {item.statut} </p>
+                <p> {item.prix} € </p>
+              </summary>
+              <p>{item.poids_kg} kg</p>
+              <p>{item.etat_arrivee}</p>
+              <p>{item.date_mise_rayon}</p>
+              <p>{item.categorie}</p>
+            </details>
           </li>
         ))}
       </ul>
     </section>
   );
 }
+
 
 export default Objet;
