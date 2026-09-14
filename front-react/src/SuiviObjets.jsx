ea63d76eedb917035b3123a06874201c41c561c2
Rémy L <remy.lagarre@gmail.com>
2026-09-14 10:35:12 +0200

    fix:suivi

--- front-react/src/SuiviObjets.jsx
+++ front-react/src/SuiviObjets.jsx
@@ -42,47 +42,19 @@ function SuiviObjets() {
                     <li><label className='liste'>Etat : </label>
                         <select className='select' onChange={(e) => {setNewEtat(e.target.value)}}>{etat_obj.map((etat) =>
                             {
-                                return etat === monObjet.etat_arrivee ? <option value={etat} selected>{etat}</option> : <option value={etat} >{etat}</option>
+                                return etat === monObjet.etat_arrivee ? <option value={modifier_statut(etat)} selected>{modifier_statut(etat)}</option> : <option value={modifier_statut(etat)} >{modifier_statut(etat)}</option>
                             }
                         )}
                         </select></li>
                     <li><label className='liste'>Statut : </label>
                         <select  className='select' onChange={(e) => {setNewStatut(e.target.value)}}>{statut_obj.map((statut) =>
                             {
-                                return statut === monObjet.statut ? <option value={statut} selected>{statut}</option> : <option value={statut}>{statut}</option>
+                                return statut === monObjet.statut ? <option value={modifier_statut(statut)} selected>{modifier_statut(statut)}</option> : <option value={modifier_statut(statut)}>{modifier_statut(statut)}</option>
                             }
                         )}
                         </select></li>
                     <li><label className='liste'>Prix : </label><input placeholder={monObjet.prix + "€"} onChange={(e) => {setNewPrice(e.target.value)} } /></li>
 
-            <h1>Suivi Objet</h1>
-            
-            <label>Numéro : </label><label>{monObjet.id}</label><br/>
-            <label>Nom : </label><label>{monObjet.libelle}</label><br/>
-            <label>Poids (Kg) : </label><label>{monObjet.poids_kg}</label><br/>
-            <label>Catégorie : </label><label>{monObjet.categorie}</label><br/>
-            <label>Etat : </label>
-                <select onChange={(e) => {setNewEtat(e.target.value)}}>{etat_obj.map((etat) =>
-                    {
-                        return etat === monObjet.etat_arrivee ? <option value={modifier_statut(etat)} selected>{modifier_statut(etat)}</option> : <option value={modifier_statut(etat)} >{modifier_statut(etat)}</option>
-                    }
-                )}
-                </select><br/>
-            <label>Statut : </label>
-                <select  onChange={(e) => {setNewStatut(e.target.value)}}>{statut_obj.map((statut) =>
-                    {
-                        return statut === monObjet.statut ? <option value={modifier_statut(statut)} selected>{modifier_statut(statut)}</option> : <option value={modifier_statut(statut)}>{modifier_statut(statut)}</option>
-                    }
-                )}
-                </select><br/>
-            <label>Prix : </label><input placeholder={monObjet.prix} onChange={(e) => {setNewPrice(e.target.value)} } /><br/>
-
-            <button onClick={() => {
-                const data = {etat_arrivee:newEtat, statut:newStatut, prix:newPrix};
-                
-                const envoyerDonnees = async () => {
-                    const path = location.pathname + '/statut/';
-                    
                 </ul>
                 <span className='button' onClick={() => {
                         const data = {etat_arrivee:newEtat, statut:newStatut, prix:newPrix};
