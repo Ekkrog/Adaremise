df40a38eefa6ee41bc4a3a0d3647d7110044257a
leika127 <afrx35@gmail.com>
2026-09-17 11:46:56 +0200

    fix suppression et ajout bénévoles

--- front-react/src/Benevoles.jsx
+++ front-react/src/Benevoles.jsx
@@ -4,20 +4,18 @@ import { getData } from './assets/utils.js';
 import { useLocation, useParams } from 'react-router-dom';
 import './variables.css';
 import logo from './assets/img/logo-la-remise.png';
-import AjoutBenevole from './AjoutBenevole.jsx';
 
 function Benevoles({connecte, setConnecte, benevoleChoisi, setBenevoleChoisi}) {
 
     const [ listeBenevoles, setListeBenevoles ] = useState([]);
-    const [ creer, setCreer ] = useState(false);
     const location = useLocation();
 
-    const chargerDonnees = async () => {
-        const benevoles = await getData('/benevoles/');
-        setListeBenevoles(benevoles);
-    }
-
     useEffect(() => {
+        const chargerDonnees = async () => {
+            const benevoles = await getData('/benevoles/');
+            setListeBenevoles(benevoles);
+        }
+
         chargerDonnees();
     }, [])
 
@@ -25,48 +23,22 @@ function Benevoles({connecte, setConnecte, benevoleChoisi, setBenevoleChoisi}) {
     <>
         <img className='logo-connect' src={logo} />
         <section className='benevoles'>
-            {!creer && (
-                <>
-                    <select onChange={(e) => {
-                        setBenevoleChoisi(e.target.value);
-                    }} className='select'>
-                        <option value=''>Qui êtes-vous ?</option>
-                        {listeBenevoles.map((b) => {
-                            return <option key={b.id} value={b.nom + " " + b.prenom}>{b.nom} {b.prenom}</option>
-                        })}
-                    </select>
+            <select onChange={(e) => {
+                setBenevoleChoisi(e.target.value);
+            }} className='select'>
+                <option value=''>Qui êtes-vous ?</option>
+                {listeBenevoles.map((b) => {
+                    return <option key={b.id} value={b.nom + " " + b.prenom}>{b.nom} {b.prenom}</option>
+                })}
+            </select>
 
-                    <span className='button tiny' onClick={() => {
-                        setCreer(true);
-                    }}>
-                        ✙
-                    </span>
-
-                    <span className='button' onClick={() => {
-                        if(benevoleChoisi!=0){
-                            setConnecte(true);
-                            localStorage.setItem("connecte","true");
-                            localStorage.setItem("benevoleChoisi", benevoleChoisi)
-                        }
-                    }}>Me connecter</span>
-                </>
-            )}
-
-            {creer && (
-                <>
-                    <AjoutBenevole onBenevoleAjoute={(nouveau) => {
-                        chargerDonnees();
-                        setBenevoleChoisi(nouveau.nom + " " + nouveau.prenom);
-                        setCreer(false);
-                    }} />
-
-                    <span className='button tiny' onClick={() => {
-                        setCreer(false);
-                    }}>
-                        Annuler
-                    </span>
-                </>
-            )}
+            <span className='button' onClick={() => {
+                if(benevoleChoisi!=0){
+                    setConnecte(true);
+                    localStorage.setItem("connecte","true");
+                    localStorage.setItem("benevoleChoisi", benevoleChoisi)
+                }
+            }}>Me connecter</span>
         </section>
 
     </>
--- front-react/src/GestionBenevoles.jsx
+++ front-react/src/GestionBenevoles.jsx
@@ -1,10 +1,13 @@
 import { useState, useEffect } from "react";
 import { getData } from "./assets/utils.js";
 import "./GestionBenevoles.css";
+import AjoutBenevole from './AjoutBenevole.jsx';
 
 function GestionBenevoles() {
     const [listeBenevoles, setListeBenevoles] = useState([]);
     const [erreur, setErreur] = useState("");
+    const [idAConfirmer, setIdAConfirmer] = useState(null);
+    const [creer, setCreer] = useState(false);
 
     const chargerBenevoles = async () => {
         const benevoles = await getData("/benevoles/");
@@ -15,10 +18,11 @@ function GestionBenevoles() {
         chargerBenevoles();
     }, []);
 
-    const supprimerBenevole = async (id) => {
+    const confirmerSuppression = async (id) => {
         try {
             await getData(`/benevoles/${id}`, "DELETE");
             setListeBenevoles(listeBenevoles.filter((b) => b.id !== id));
+            setIdAConfirmer(null);
         } catch (err) {
             console.error(err);
             setErreur("Erreur lors de la suppression");
@@ -29,16 +33,57 @@ function GestionBenevoles() {
         <>
             <h2>Gestion des bénévoles</h2>
             {erreur && <p className="erreur">{erreur}</p>}
+
+            {!creer && (
+                <span className="button tiny" onClick={() => setCreer(true)}>
+                    ✙ Ajouter un bénévole
+                </span>
+            )}
+
+            {creer && (
+                <AjoutBenevole onBenevoleAjoute={() => {
+                    chargerBenevoles();
+                    setCreer(false);
+                }} />
+            )}
+            {creer && (
+                <span className="button tiny" onClick={() => setCreer(false)}>
+                    Annuler
+                </span>
+            )}
+
             <div className="liste_benevoles">
                 {listeBenevoles.map((b) => (
                     <div key={b.id} className="ligne_benevole">
-                        <span>{b.nom} {b.prenom}</span>
-                        <span
-                            className="bouton_supprimer"
-                            onClick={() => supprimerBenevole(b.id)}
-                        >
-                            🗑
-                        </span>
+                        {idAConfirmer === b.id ? (
+                            <div className="confirmation_suppression">
+                                <span>Supprimer {b.nom} {b.prenom} ?</span>
+                                <div className="boutons_confirmation">
+                                    <span
+                                        className="bouton_annuler"
+                                        onClick={() => setIdAConfirmer(null)}
+                                    >
+                                        Annuler
+                                    </span>
+                                    <span
+                                        className="bouton_confirmer"
+                                        onClick={() => confirmerSuppression(b.id)}
+                                    >
+                                        Confirmer
+                                    </span>
+                                </div>
+                            </div>
+                        ) : (
+                            <>
+                                <span>{b.nom} {b.prenom}</span>
+                                <span
+                                    className="bouton_supprimer"
+                                    onClick={() => setIdAConfirmer(b.id)}
+                                >
+                                    🗑
+                                </span>
+                            </>
+                        )}
                     </div>
                 ))}
             </div>
